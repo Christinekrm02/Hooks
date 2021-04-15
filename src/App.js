dd79eb4b34570b384e282d6319ede8eecc13c049
Christine Maynard <christinekrm02@gmail.com>
2021-04-15 17:19:20 -0400

    Create a hook for a counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ClassCounter from "./components/ClassCounter.jsx";
+import HookCounter from "./components/HookCounter.jsx";
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      {/* <ClassCounter /> */}
+      <HookCounter />
     </div>
   );
 }
